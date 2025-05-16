9f53b4247462b61a8749dd1bb9130308a2b93023
Kishan Agarwal <agarwalkishan018@gmail.com>
2025-05-16 13:28:25 +0530

    DELETE page folder

---
+++ app/projects/page.js
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-4xl font-bold">My Projects</h1>
+      <p className="mt-4 text-lg">Here are some of my recent works.</p>
+    </div>
+  );
+}
